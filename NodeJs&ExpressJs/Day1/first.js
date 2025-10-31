1ea76f672517e318af5b80bc43f2419b02c47cc3
Mahesh Rajpurohit <contact@mahesh.icu>
2025-10-31 23:12:31 +0530

    NodeJs Day 1

---
+++ NodeJs&ExpressJs/Day1/first.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+// const data = fs.readFileSync('./dummy.txt',{encoding: 'utf8'});
+// console.log(data);
+
+
+// fs.writeFileSync('./dummy.txt', 'Hello World');
+
+
+// fs.appendFileSync('./dummy.txt', ' this is added Content', {encoding: 'utf8'});
+
+
+// fs.readFile('./dummy.txt', {encoding: 'utf8'}, (err, data) => {
+//     if (err===null){
+//         console.log(data);
+//     }
+// })
+// console.log("checking if async");
+
+
+// fs.writeFile('./dummy.txt', 'Hello World', (err) => {
+//     if (!err){
+//         console.log("File writing success");
+//     }
+// })
+
+// fs.unlink("./dummy.txt",()=> {
+//     console.log("Deleted")
+// })
+
