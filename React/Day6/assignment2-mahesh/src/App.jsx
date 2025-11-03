3e6be786b1981dd025b3ef06dcde199c2d8bf639
Mahesh Rajpurohit <contact@mahesh.icu>
2025-11-03 11:49:23 +0530

    Made it Dynamic

--- React/Day6/assignment2-mahesh/src/App.jsx
+++ React/Day6/assignment2-mahesh/src/App.jsx
@@ -1,12 +1,46 @@
+import { useState } from "react"
 import Student from "./components/student"
 
 function App() {
+  const [students, setStudents] = useState([])
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [course, setCourse] = useState("")
+
+  function addStudent() {
+    if (name && age && course) {
+      const newStudent = { name, age, course }
+      setStudents([...students, newStudent])
+      setName("")
+      setAge("")
+      setCourse("")
+    }
+  }
+
   return (
     <div>
       <h2>Student List</h2>
-      <Student name="Mahesh" age="19" course="CSE" />
-      <Student name="Riya" age="20" course="IT" />
-      <Student name="Amit" age="21" course="AIML" />
+
+      <input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        placeholder="Age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <input
+        placeholder="Course"
+        value={course}
+        onChange={(e) => setCourse(e.target.value)}
+      />
+      <button onClick={addStudent}>Add Student</button>
+
+      {students.map((s, index) => (
+        <Student key={index} name={s.name} age={s.age} course={s.course} />
+      ))}
     </div>
   )
 }
